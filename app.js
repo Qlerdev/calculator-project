74e1e77a2b2e3a3e6c088410a9e6ac62bbf3aeb0
Punnakan Punsiri <punnakan2509@gmail.com>
2024-09-14 01:26:56 +0700

    add displaying value + collecting value from user

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 let buttons = document.getElementsByTagName("button");
-let input = document.getElementById("input");
+let displayInput = document.getElementById("input");
+const deleteAll = document.getElementById("delete-all");
+const deleteRecent = document.getElementById("delete-recent");
+const percent = document.getElementById("percent");
+const increase = document.getElementById("increase");
+const decrease = document.getElementById("decrease");
+const multiply = document.getElementById("multiply");
+const divide = document.getElementById("divide");
 
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", function () {
-    console.log("buttons : " + buttons[i].textContent + " ได้ทำการกด");
-    
-    // เพิ่มข้อความของปุ่มที่กดเข้าไปใน input div
-    input.textContent += buttons[i].textContent;
-  });
+let inputvalue = []; //เก็บค่าที่ user ทำการกด
+
+function collectInputValue() {
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+      // เพิ่มข้อความของปุ่มที่กดเข้าไปใน input div
+      displayInput.textContent += buttons[i].textContent;
+      //เอาค่ามารวมกันใน array
+      inputvalue.push(buttons[i].value);
+      console.log(inputvalue);
+    });
+  }
 }
+
+increase.addEventListener("click", () => {});
+collectInputValue();
